d3fecc55e9a0a9045ac2d38bda2904d42e2d456d
rogersmb77 <rogersmb77@gmail.com>
2017-04-27 01:02:43 -0400

    Create FourRowSolitaireTest.java

---
+++ FourRowSolitaire/src/test/FourRowSolitaireTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.testng.Assert.assertNotNull;
+
+import java.awt.event.ActionEvent;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import FourRowSolitaire.FourRowSolitaire;
+
+public class FourRowSolitaireTest {
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @Test
+  public void FourRowSolitaire()throws Exception {
+		
+		assertNotNull(FourRowSolitaire.class);
+		FourRowSolitaire target = new FourRowSolitaire();
+		assertNotNull(target);
+  }
+
+  @Test
+  public void actionPerformedActionEvent() throws Exception {
+		
+		FourRowSolitaire target = new FourRowSolitaire();
+		ActionEvent e = null;
+		target.actionPerformed(e);
+	}
+
+  @Test
+  public void checkForUpdate() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void loadData() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void mainStringArray() throws Exception {
+		
+		String[] args = new String[] {};
+		FourRowSolitaire.main(args);
+	}
+
+}
